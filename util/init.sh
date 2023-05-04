41a98df09317bfa24685a9801dd016f0d38a1248
Taylor Smith <tsmith@tsmithcreative.com>
2023-05-04 21:10:36 +0000

    #2: Add docker builds and a wp-config file into the init script

--- util/init.sh
+++ util/init.sh
@@ -16,22 +16,34 @@ git clone git@github.com:tsmith512/routenotfound.git apache/docroot
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # And we need to use it immediately
-source ~/.bashrc
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
 
 cd apache/docroot/wp-content/themes/rnf-alfa
 git checkout dev
 ln -s ../../../../../.env .env
 
-# @TODO: This didn't work, nvm was still undefined
-echo "Navigate to the theme, nvm install/use, npm install, npx gulp"
+echo "## Building theme assets"
 nvm install
 nvm use
 npm install
 npx gulp
 
-# @TODO: Need to make this in a way that doesn't hard-code paths
-#sudo ln -s /home/tsmith/rnf-deploy/rnf.service /etc/systemd/system/rnf.service
-echo Link the systemd service unit and enable
+cd ../../../../..
 
-echo Next, start the Compose stack, pull a backup archive, and run util/restore.sh
+cp apache/docroot/wp-config-sample.php apache/docroot/wp-config.php
+rm -f apache/docroot/salt.php
+touch apache/docroot/salt.php
+echo "<?php" > apache/docroot/salt.php
+curl https://api.wordpress.org/secret-key/1.1/salt/ >> apache/docroot/salt.php
 
+echo "## Building Docker conainers"
+docker compose create --build
+
+echo "## Installing systemd service unit"
+sudo ln -s $(realpath rnf.service) /etc/systemd/system/rnf.service
+sudo systemctl enable rnf
+sudo systemctl start rnf
+
+echo "## Running restore script"
+util/restore.sh
